1ed9746d568e842c0787bf24d525fe4d67386e90
Kaustav Banerjee <kaustav@opencraft.com>
2021-12-01 14:11:41 +0530

    changed from mongoexport to pymongo

--- exporter/tasks.py
+++ exporter/tasks.py
@@ -4,9 +4,14 @@ import logging
 import os
 import subprocess
 import distutils
+import json
+
+from bson.json_util import dumps
 
 import boto3
 import botocore
+
+from pymongo import MongoClient
 
 from opaque_keys.edx.keys import CourseKey
 
@@ -171,33 +176,37 @@ class MongoTask(Task):
     NAME = NotSet
     QUERY = NotSet
     EXT = 'mongo'
-    CMD = """
-    mongoexport
-      --host={mongo_host}
-      --db={mongo_db}
-      --username={mongo_user}
-      --password={mongo_password}
-      --collection={mongo_collection}
-      --query='{query}'
-      --out={filename}
-      >&2
-    """
+
+    @classmethod
+    def constructMongoURI(*args, **kwargs):
+
+        uri = "mongodb://"
+        
+        if kwargs.get("mongo_user").strip() and kwargs.get("mongo_password").strip():
+            uri = f"{uri}{kwargs.get('mongo_user').strip()}:{kwargs.get('mongo_password').strip()}@"
+
+        return f"{uri}{kwargs.get('mongo_host')}"
+
 
     @classmethod
     def run(cls, filename, dry_run, **kwargs):
         super(MongoTask, cls).run(filename, dry_run, **kwargs)
 
+        mongo_uri = cls.constructMongoURI(**kwargs)
+
         query = clean_command(cls.QUERY).format(**kwargs)
 
         log.debug(query)
 
-        cmd = clean_command(cls.CMD)
-        cmd = cmd.format(filename=filename, query=query, **kwargs)
-
         if dry_run:
             print('MONGO: {0}'.format(query))
         else:
-            execute_shell(cmd, **kwargs)
+            collection = MongoClient(mongo_uri)[kwargs.get("mongo_db")][kwargs.get("mongo_collection")]
+            cursor = collection.find(json.loads(query))
+            with open(filename, 'w') as file:
+                for document in cursor:
+                    file.write(dumps(document))
+                    file.write("\n")
 
 
 class DjangoAdminTask(Task):
