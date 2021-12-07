4a84b4d2b7aae56213c8d83af53c3c05e698dd5e
Kaustav Banerjee <kaustav@opencraft.com>
2021-12-07 10:15:33 +0530

    added org level task

--- exporter/course_export.py
+++ exporter/course_export.py
@@ -45,6 +45,8 @@ Options:
   --sql-db=<db>             The sql db to query
 
   --secret-key=<secert-key> The secret key to calculate user id hash
+
+  --organization=<org-name> The name of the organization
 """
 
 
@@ -61,7 +63,7 @@ import boto3
 from opaque_keys.edx.keys import CourseKey
 from opaque_keys import InvalidKeyError
 
-from exporter.tasks import CourseTask, FatalTaskError
+from exporter.tasks import CourseTask, FatalTaskError, OrgTask
 from exporter.main import run_tasks, archive_directory, get_all_courses, _get_selected_tasks
 from exporter.config import setup, get_config_for_course
 from exporter.util import make_temp_directory, merge
@@ -74,13 +76,28 @@ def main():
 
     courses = get_courses(general_config)
 
-    for course in courses:
-        config = get_config_for_course(general_config, course)
+    with make_org_directory(general_config) as temp_directory:
+        export_org_data(general_config, courses, temp_directory)
 
-        with make_course_directory(config, course) as temp_directory:
-            results = export_course_data(config, temp_directory)
-            root_dir = archive_directory(temp_directory)
-            upload_files_or_dir(config, root_dir)
+        for course in courses:
+            config = get_config_for_course(general_config, course)
+            course_directory = os.path.join(temp_directory, get_filename_safe_course_id(course))
+            os.mkdir(course_directory)
+            results = export_course_data(config, course_directory)
+        
+        root_dir = archive_directory(temp_directory)
+        upload_files_or_dir(config, root_dir)
+
+
+def export_org_data(config, courses, destination):
+    kwargs = merge(config['values'], {})
+    kwargs['courses'] = courses
+    kwargs['work_dir'] = destination
+    tasks_from_options = kwargs.get('tasks', [])
+
+    org_tasks = _get_selected_tasks(OrgTask, tasks_from_options, [])
+    run_tasks(org_tasks, **kwargs)
+
 
 def get_courses(config):
 
@@ -138,17 +155,17 @@ def upload_files_or_dir(config, results_directory, sub_directory=None):
 def upload_file(config, filepath, filename):
     bucket = config['output_bucket']
     prefix = config['output_prefix'] or ''
-    filename_safe_course_id = get_filename_safe_course_id(config['course'])
+    organization = config['organization']
     output_date = str(datetime.date.today())
 
-    s3_target = '{prefix}_{course}/{date}/{name}'.format(
+    s3_target = '{prefix}_{org}/{date}/{name}'.format(
         prefix=prefix,
-        course=filename_safe_course_id,
+        org=organization,
         date=output_date,
         name=filename
     )
 
-    target = f's3://{s3_target}'
+    target = f's3://{bucket}/{s3_target}'
 
     log.info('Uploading file %s to %s', filepath, target)
     s3_client = boto3.client('s3')
@@ -156,20 +173,20 @@ def upload_file(config, filepath, filename):
 
 
 @contextmanager
-def make_course_directory(config, course):
-    filename_safe_course = get_filename_safe_course_id(course)
-    course_dir = config['work_dir']
+def make_org_directory(config):
+    org_dir = config['values']['work_dir']
+    organization = config['values']['organization']
 
-    prefix = '{0}_'.format(filename_safe_course)
+    prefix = '{0}_'.format(organization)
 
-    with make_temp_directory(prefix=prefix, directory=course_dir) as temp_dir:
+    with make_temp_directory(prefix=prefix, directory=org_dir) as temp_dir:
         # create working directory
         today = str(datetime.date.today())
-        dir_name = '{name}-{date}'.format(name=filename_safe_course, date=today)
-        course_dir = os.path.join(temp_dir, dir_name)
-        os.mkdir(course_dir)
+        dir_name = '{name}-{date}'.format(name=organization, date=today)
+        org_dir = os.path.join(temp_dir, dir_name)
+        os.mkdir(org_dir)
 
-        yield course_dir
+        yield org_dir
 
 def get_filename_safe_course_id(course_id, replacement_char='_'):
     """
